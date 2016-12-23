ae5dc4bdbe762a04bab729ed294f43fb277e16b4
tiansongtao <769001510@qq.com>
2016-12-23 19:40:08 +0800

    tian song

--- js/index.js
+++ js/index.js
@@ -1,31 +1,15 @@
+
+
 function openJianli (){
-	var iframeObj = document.getElementsByClassName('content-index');
-	var bt = document.getElementsByClassName('bt-xuan-xiang-name');
-	for(var i=0;i<bt.length;i++){
-		bt[i].index=i;
-		iframeObj[i].style.display='none';
-	}
-	iframeObj[0].style.display='block';
-	
+
+	window.location.href='html/jianli.html';
 }
 
 
 function openGithub (){
-		var iframeObj = document.getElementsByClassName('content-index');
-	var bt = document.getElementsByClassName('bt-xuan-xiang-name');
-	for(var i=0;i<bt.length;i++){
-		bt[i].index=i;
-		iframeObj[i].style.display='none';
-	}
-	iframeObj[1].style.display='block';
+    window.location.href='https://github.com/tiansongtao';
 }
 
 function openBoke (){
-		var iframeObj = document.getElementsByClassName('content-index');
-	var bt = document.getElementsByClassName('bt-xuan-xiang-name');
-	for(var i=0;i<bt.length;i++){
-		bt[i].index=i;
-		iframeObj[i].style.display='none';
-	}
-	iframeObj[2].style.display='block';
+    window.location.href='html/jianli.html';
 }
